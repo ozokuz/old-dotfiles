fee6d053c399a7b622ec038f6d3008aa52cac4af
Ozoku <19756856+ozokuz@users.noreply.github.com>
2022-04-04 15:01:49 +0300

    remove nvim-lsp-ts-utils

--- linux/nvim/.config/nvim/lua/ozokuz/lsp/providers/tsserver.lua
+++ linux/nvim/.config/nvim/lua/ozokuz/lsp/providers/tsserver.lua
@@ -6,11 +6,11 @@ function M.on_attach(client, bufnr)
   client.resolved_capabilities.document_formatting = false
   client.resolved_capabilities.document_range_formatting = false
 
-  local ts_utils = require('nvim-lsp-ts-utils')
+  --local ts_utils = require('nvim-lsp-ts-utils')
 
-  ts_utils.setup {}
+  --ts_utils.setup {}
 
-  ts_utils.setup_client(client)
+  --ts_utils.setup_client(client)
 
   orig_on_attach(client, bufnr)
 end

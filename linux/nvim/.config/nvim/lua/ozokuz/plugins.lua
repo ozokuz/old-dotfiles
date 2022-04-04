21885cc8307fe287383ef11431ed03a22c4a9e91
Ozoku <19756856+ozokuz@users.noreply.github.com>
2022-04-04 14:56:06 +0300

    initialize packer when opening neovim for the first time

--- linux/nvim/.config/nvim/lua/ozokuz/plugins.lua
+++ linux/nvim/.config/nvim/lua/ozokuz/plugins.lua
@@ -1,3 +1,9 @@
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+end
+
 return require('packer').startup(function(use)
   -- Make packer manage itself
   use 'wbthomason/packer.nvim'
@@ -131,4 +137,8 @@ return require('packer').startup(function(use)
 
   -- Status Line
   use 'nvim-lualine/lualine.nvim'
+
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)

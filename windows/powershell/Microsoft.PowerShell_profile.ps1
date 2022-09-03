b9e0654e7b1e666cc90561e8df9d97a365aaee92
Ozoku <19756856+ozokuz@users.noreply.github.com>
2022-09-04 00:27:29 +0300

    add powershell profile

---
+++ windows/powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,69 @@
+# Encoding
+[console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
+
+# Imports
+Import-Module posh-git
+Import-Module Terminal-Icons
+Import-Module cd-extras
+Import-Module PSFzf
+
+# Enable Prediction
+Set-PSReadLineOption -PredictionSource History
+
+# Fzf
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
+
+# Custom Commands
+## Java
+function java8 {
+  $ENV:JAVA_HOME = "C:\Program Files\Eclipse Adoptium\jdk-8.0.312.7-hotspot"
+  $ENV:PATH = "$ENV:JAVA_HOME\bin;$ENV:PATH"
+
+  java -version
+}
+
+function java17 {
+  $ENV:JAVA_HOME = "C:\Program Files\Eclipse Adoptium\jdk-17.0.1.12-hotspot"
+  $ENV:PATH = "$ENV:JAVA_HOME\bin;$ENV:PATH"
+
+  java -version
+}
+
+## Windows Terminal - New Tab in PWD
+function th {
+  wt.exe -w 0 nt pwsh -nologo -wd $pwd
+}
+
+## Simplify yt-dlp usage
+function ytdlm {
+  yt-dlp.exe -x --audio-format mp3 --output "%(title)s.%(ext)s" $args
+}
+
+## Which
+function which ($command) {
+  Get-Command -Name $command -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+}
+
+## z w/ fzf
+function zz {
+  Invoke-FuzzyZLocation
+}
+
+# Aliases
+Set-Alias v nvim
+Set-Alias l ls
+Set-Alias g git
+Set-Alias lg lazygit
+
+# Completion
+packwiz completion powershell | Out-String | Invoke-Expression
+
+# Prompt
+Invoke-Expression (&starship init powershell)
+
+# z
+Import-Module ZLocation
+
+# Fix Prompt
+clear
+

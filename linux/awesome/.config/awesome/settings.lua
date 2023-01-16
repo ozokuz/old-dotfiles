de56958f2d985cdb627a56cbc00862f743dc65b3
Ozoku <19756856+ozokuz@users.noreply.github.com>
2023-01-16 23:01:08 +0200

    switch to thunar for file manager

--- linux/awesome/.config/awesome/settings.lua
+++ linux/awesome/.config/awesome/settings.lua
@@ -3,7 +3,7 @@ local settings = {
   editor = os.getenv('EDITOR') or 'nvim',
   browser = os.getenv('BROWSER') or 'brave',
   task_manager = 'btop',
-  gui_file_manager = 'nemo',
+  gui_file_manager = 'thunar',
 }
 
 return settings

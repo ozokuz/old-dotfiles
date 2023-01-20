71e225555593315b8d607daba4fd8f7029337d6e
Ozoku <19756856+ozokuz@users.noreply.github.com>
2023-01-21 00:09:09 +0200

    fix default apps

--- linux/awesome/.config/awesome/settings.lua
+++ linux/awesome/.config/awesome/settings.lua
@@ -2,7 +2,7 @@ local settings = {
   terminal = 'alacritty',
   editor = os.getenv('EDITOR') or 'nvim',
   browser = os.getenv('BROWSER') or 'brave',
-  task_manager = 'btop',
+  task_manager = 'alacritty -e btop',
   gui_file_manager = 'thunar',
 }
 

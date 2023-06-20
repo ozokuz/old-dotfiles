007f5afea7bde750f7f19ec520607d15922ecefa
Ozoku <19756856+ozokuz@users.noreply.github.com>
2023-06-20 19:23:52 +0300

    change default layout to tile

--- linux/awesome/.config/awesome/configuration/layouts.lua
+++ linux/awesome/.config/awesome/configuration/layouts.lua
@@ -2,9 +2,9 @@ local awful = require('awful')
 
 tag.connect_signal('request::default_layouts', function()
   awful.layout.append_default_layouts({
+    awful.layout.suit.tile,
     awful.layout.suit.spiral.dwindle,
     awful.layout.suit.spiral,
-    awful.layout.suit.tile,
     awful.layout.suit.floating,
   })
 end)
